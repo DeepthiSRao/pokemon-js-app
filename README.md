7262c51971ad19a23202362e275daf35abccaff7
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2021-09-23 17:17:04 -0700

    Added Pokemon data to Pokemonlist JS

---
+++ README.md
@@ -0,0 +1,3 @@
+#Pokemon App
+
+Is a simple web application with HTML, CSS, and Javascript that loads data from external API and enables the viewing of data points in details.
--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-alert('Hello World');
-
-//Writing message to the screen
-var favoriteFood = "Pizza😃";
-document.write(`My favourite foods is ${favoriteFood} `); 
+var pokemonList = [
+    {
+        name : 'Bulbasaur',
+        height: 7,
+        types: ['grass' , 'poison']
+    },
+    {
+        name : 'Ivysaur',
+        height: 10,
+        types: ['grass' , 'poison']
+    },
+    {
+        name : 'Venusaur',
+        height: 20,
+        types: ['grass' , 'poison']
+    },
+    {
+        name : 'Charmander',
+        height: 6,
+        types: 'fire'
+    },
+    {
+        name : 'Charmeleon',
+        height: 11,
+        types: 'fire'
+    },
+    {
+        name : 'Charizard',
+        height: 17,
+        types: ['fire', 'flying']
+    },
+];
